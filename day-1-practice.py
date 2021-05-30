327dc0223312746026c5d0ae860860bb93dcb50e
Jeff Chiu <jwchiu82@gmail.com>
2021-05-30 11:43:41 -0700

    complete day 1-4 exercise

--- day-1-practice.py
+++ day-1-practice.py
@@ -1,2 +1,22 @@
-name = input("What is your name? ")
-print (name + " is a silly goose")
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+newB = a
+newA = b
+a = newA
+b = newB
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
