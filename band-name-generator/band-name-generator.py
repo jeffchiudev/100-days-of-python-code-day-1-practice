4e93f5d04a886ceb24eafc30daf2ce6d4e2d2da3
Jeff Chiu <jwchiu82@gmail.com>
2021-05-30 11:52:55 -0700

    complete day 1 band name generator

---
+++ band-name-generator/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("What was the name of the city you grew up in?\n")
+pet = input("What was the name of your first pet?\n")
+print("Your new band name is: " + city + " " + pet + "!")
